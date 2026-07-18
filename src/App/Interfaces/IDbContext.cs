722512276286576715c8236ab8916da70f0a16a1
Zynres <killp8918@gmail.com>
2026-07-18 09:01:13 +0300

    feat: add abstraction for db context: create interface in app

---
+++ src/App/Interfaces/IDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace App.Interfaces;
+
+public interface IDbContext
+{
+    DbSet<Employee> Employees { get; set; }
+    DbSet<Request> Requests { get; set; }
+    DbSet<Department> Departments { get; set; }
+    DbSet<Position> Positions { get; set; }
+
+    Task<int> SaveChangesAsync();
+}
