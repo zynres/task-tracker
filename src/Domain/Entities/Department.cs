5cd452cc7b6d676be358ce4ef2f297ddc383a832
Zynres <killp8918@gmail.com>
2026-07-18 09:03:11 +0300

    feat: create new entity department and position

---
+++ src/Domain/Entities/Department.cs
@@ -0,0 +1,8 @@
+namespace Domain.Entities;
+
+public class Department
+{
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+}
---
+++ src/Domain/Entities/Position.cs
@@ -0,0 +1,8 @@
+namespace Domain.Entities;
+
+public class Position
+{
+    public int Id { get; set; }
+
+    public required string Name;
+}
