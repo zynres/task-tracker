a6b5ce86705b3b676ffa6581a8a6691e3dd01e18
Zynres <killp8918@gmail.com>
2026-07-18 09:23:22 +0300

    feat: add new cs project load test and class program

---
+++ test/LoadTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using NBomber.Http.CSharp;
+using NBomber.CSharp;
+
+namespace LoadTest;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var client = Http.CreateDefaultClient();
+
+        var scenario =
+            Scenario.Create("Post Employees", async context =>
+            {
+                var request = Http.CreateRequest(
+                    "POST",
+                    "http://localhost:5100/api/employee/count/1");
+
+                return await Http.Send(client, request);
+            })
+            .WithLoadSimulations(
+                Simulation.Inject(
+                    rate: 10000,
+                    interval: TimeSpan.FromSeconds(1),
+                    during: TimeSpan.FromSeconds(30))
+            );
+
+        NBomberRunner
+            .RegisterScenarios(scenario)
+            .Run();
+    }
+}
