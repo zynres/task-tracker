58ccd965c42a970c21ea97be2566fdf5a5c6970d
Zynres <killp8918@gmail.com>
2026-07-20 11:46:38 +0300

    feat: implement the department controller

--- src/API/Controllers/DepartmentController.cs
+++ src/API/Controllers/DepartmentController.cs
@@ -1,4 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using App.Features.Department.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using Domain.Entities;
 using App.Interfaces;
 
 namespace API.Controllers;
@@ -17,8 +20,49 @@ public class DepartmentController : ControllerBase
     }
 
     [HttpPost("count/{count}")]
-    public async Task<IActionResult> Create(int count)
+    public async Task<ActionResult<DepartmentDto>> Create(int count, [FromBody] string name)
     {
-        return Ok(count);
+        var departments = new Department[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            var department = new Department()
+            {
+                Name = name
+            };
+            departments[i] = department;
+        }
+
+        context.Departments.AddRange(departments);
+
+        await context.SaveChangesAsync();
+
+        var departmentDtos = new DepartmentDto[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            Department department = departments[i];
+            
+            departmentDtos[i] = new DepartmentDto(
+                department.Id, 
+                department.Name);
+        }
+
+        return Ok(departmentDtos);
+    }
+
+    [HttpGet("{departmentId}")]
+    public async Task<ActionResult<DepartmentDto>> GetDepartment(int departmentId)
+    {
+        var departmentDto = await context.Departments
+            .AsNoTracking()
+            .Where(department => department.Id == departmentId)
+            .Select(department => new DepartmentDto(department.Id, department.Name))
+            .FirstOrDefaultAsync();
+
+        if (departmentDto == null)
+            return NotFound("Department not found");
+
+        return Ok(departmentDto);
     }
 }
