f39ab24a3ff092d1baa48bfad893c9b65902cda5
Zynres <killp8918@gmail.com>
2026-07-20 11:47:05 +0300

    feat: implement the position controller

--- src/API/Controllers/PositionController.cs
+++ src/API/Controllers/PositionController.cs
@@ -1,4 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using App.Features.Position.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using Domain.Entities;
 using App.Interfaces;
 
 namespace API.Controllers;
@@ -17,8 +20,49 @@ public class PositionController : ControllerBase
     }
 
     [HttpPost("count/{count}")]
-    public async Task<IActionResult> Create(int count)
+    public async Task<ActionResult<PositionDto>> Create(int count, [FromBody] string name)
     {
-        return Ok(count);
+        var positions = new Position[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            var position = new Position()
+            {
+                Name = name
+            };
+            positions[i] = position;
+        }
+
+        context.Positions.AddRange(positions);
+
+        await context.SaveChangesAsync();
+
+        var positionDtos = new PositionDto[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            Position position = positions[i];
+            
+            positionDtos[i] = new PositionDto(
+                position.Id, 
+                position.Name);
+        }
+
+        return Ok(positionDtos);
+    }
+
+    [HttpGet("{positionId}")]
+    public async Task<ActionResult<PositionDto>> GetPosition(int positionId)
+    {
+        var positionDto = await context.Positions
+            .AsNoTracking()
+            .Where(position => position.Id == positionId)
+            .Select(position => new PositionDto(position.Id, position.Name))
+            .FirstOrDefaultAsync();
+
+        if (positionDto == null)
+            return NotFound("Position not found");
+
+        return Ok(positionDto);
     }
 }
